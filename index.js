a27686f069ac3d500dd1abc882dfe8aa7cdbaef3
NicoKandut <nico.kandut@gmail.com>
2018-08-12 10:23:37 +0100

    added aliases for router, some cmments

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
+// r34-json-api
+
+// require packages
 const scraperjs = require('scraperjs');
 const express = require('express');
-const app = express();
 
+// setup
+const app = express();
 const serviceUrl = 'https://rule34.xxx/index.php?page=dapi&q=index';
 process.env.PORT = process.env.PORT || 8080;
 process.env.HOST = process.env.HOST || 'http://localhost:' + process.env.PORT;
 
+// require routers
 const commentRouter = require('./commentRouter');
 const postRouter = require('./postRouter');
 
+// assign routers
 app.use('/comments', commentRouter);
 app.use('/posts', postRouter);
 
+app.use('/c', commentRouter);
+app.use('/p', postRouter);
+
+// start server
 app.listen(process.env.PORT, function () {
   console.log('App listening on port ' + process.env.PORT + '.');
 });
